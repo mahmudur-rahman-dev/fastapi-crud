ccab06863c39e9773c917e822c8678fc1f8d4be2
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2024-06-04 00:31:04 +0600

    refactor

--- app/models/item_model.py
+++ app/models/item_model.py
@@ -7,12 +7,3 @@ class Item(BaseModel):
     name: str
     description: str
     price: float
-
-    # class Config:
-    #     allow_population_by_field_name = True
-    #     json_encoders = {
-    #         ObjectId: str
-    #     }
-    #     arbitrary_types_allowed = True
-    #     alias_generator = lambda s: s
-    #     allow_population_by_alias = True
